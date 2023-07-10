35e1ac574c55a46be2155b2eab1fa94e450c8860
CiureaClaudiu <ciureagabrielclaudiu@gmail.com>
2023-07-10 19:20:28 +0300

    Added more advanced code style

--- src/action/Command/Favorite.java
+++ src/action/Command/Favorite.java
@@ -11,6 +11,28 @@ public class Favorite extends Command{
 
     public static JSONObject executeAction(Input input, ActionInputData actionInputData) {
 
+//        UserInputData user = input.getUsers().stream()
+//                .filter(user1 -> user1.getUsername().equals(actionInputData.getUsername()))
+//                .findFirst().get();
+//
+//        long error = user.getFavoriteMovies().stream()
+//                            .filter(movie -> movie.equals(actionInputData.getTitle()))
+//                            .count();
+//        if (error != 0) {
+//            JSONObject jsonObject = new JSONObject();
+//            jsonObject.put("id", actionInputData.getActionId());
+//            jsonObject.put("message", "error -> " + actionInputData.getTitle() + " is already in favourite list");
+//            return jsonObject;
+//        }
+//
+//        if (!user.getHistory().containsKey(actionInputData.getTitle())) {
+//            JSONObject jsonObject = new JSONObject();
+//            jsonObject.put("id", actionInputData.getActionId());
+//            jsonObject.put("message", "error -> " + actionInputData.getTitle() + " is not seen");
+//            return jsonObject;
+//        }
+//        user.getFavoriteMovies().add(actionInputData.getTitle());
+
         boolean canAddFavorite = false;
         for (UserInputData user : input.getUsers()) {
             if (user.getUsername().equals(actionInputData.getUsername())) {
